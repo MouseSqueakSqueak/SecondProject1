fd221d1f238e92cdd087396d244bb98a8f87e307
MouseSqueakSqueak <palatence1@gmail.com>
2022-01-02 18:03:55 +0500

    deleted model person

--- app/models.py
+++ app/models.py
@@ -5,9 +5,3 @@ Definition of models.
 from django.db import models
 
 
-class Person(models.Model):
-    id = models.IntegerField (primary_key=True) # Уникальный Идентификатор пользователя в БД
-    FirstName = models.CharField(max_length=30, blank = false, null = True) # Имя
-    FamilyName = models.CharField(max_length=30, blank = false, null = True) # Фамилия
-    PatronomycName = models.CharField(max_lenght = 30, blank = true, null = True) # Отчество
-    BirthDate =models.DateField (blank = false, default=date.today) # Дата рождения
