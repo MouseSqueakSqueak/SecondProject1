d7283b4803f21970ec14d66d7a30ffde874288fe
MouseSqueakSqueak <palatence1@gmail.com>
2022-01-02 17:04:36 +0500

    CreateModels

--- app/models.py
+++ app/models.py
@@ -4,4 +4,10 @@ Definition of models.
 
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    id = models.IntegerField (primary_key=True) # Уникальный Идентификатор пользователя в БД
+    FirstName = models.CharField(max_length=30) # Имя
+    FamilyName = models.CharField(max_length=30) # Фамилия
+    PatronomycName = models.CharField(max_lenght = 30, ) # Отчество
+    BirthDate =models.DateField () # Дата рождения
