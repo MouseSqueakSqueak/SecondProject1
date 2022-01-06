d92682c12566211c5983d9ed4d18e5596d495f04
MouseSqueakSqueak <palatence1@gmail.com>
2022-01-06 17:37:03 +0500

    Comments for You

--- Person/models.py
+++ Person/models.py
@@ -3,10 +3,10 @@ import datetime
 from django.db import models
 
 class Person(models.Model):
-    FirstName = models.CharField (max_length=50)
-    SecondName = models.CharField (max_length=50)
-    PatronymicName = models.CharField (max_length=50, blank=True)
-    BirthDate = models.DateField ()
+    FirstName = models.CharField (max_length=50, verbose_name = 'Имя') ## Имя
+    SecondName = models.CharField (max_length=50, verbose_name = 'Фамилия') ## Фамилия
+    PatronymicName = models.CharField (max_length=50, blank=True, verbose_name = 'Отчетсво') ## Отчество
+    BirthDate = models.DateField (verbose_name = 'Дата рождения') ## Дата рождения 
     
     def __str__(self):
         return self.FirstName
